7a131ad4068cc042116d9013fab65865f5a9bfb4
Danick Roussel <danick3355@gmail.com>
2025-04-15 11:58:34 -0400

    Removed unused files and lines

--- New DTAPP/Repository/Interfaces/IFileExtensionRepository.cs
+++ New DTAPP/Repository/Interfaces/IFileExtensionRepository.cs
@@ -1,14 +1,13 @@
 ﻿using New_DTAPP.Models;
 
-namespace New_DTAPP.Repository.Interfaces
+namespace New_DTAPP.Repository.Interfaces;
+
+public interface IFileExtensionRepository
 {
-    public interface IFileExtensionRepository
-    {
-        Task<List<FileExtensionModel>> GetAllFileExtensionsAsync(bool activeOnly = false, bool addBlankItemForDropDown = false);
-        Task<FileExtensionModel?> GetFileExtensionByIdAsync(int id);
-        Task<FileExtensionModel?> AddFileExtensionAsync(FileExtensionModel model);
-        Task<FileExtensionModel?> UpdateFileExtensionAsync(FileExtensionModel model);
-        Task<FileExtensionModel?> RemoveFileExtensionAsync(FileExtensionModel model);
-        bool? FileExtensionExistsAsync(int id);
-    }
+    Task<List<FileExtensionModel>> GetAllFileExtensionsAsync(bool activeOnly = false, bool addBlankItemForDropDown = false);
+    Task<FileExtensionModel?> GetFileExtensionByIdAsync(int id);
+    Task<FileExtensionModel?> AddFileExtensionAsync(FileExtensionModel model);
+    Task<FileExtensionModel?> UpdateFileExtensionAsync(FileExtensionModel model);
+    Task<FileExtensionModel?> RemoveFileExtensionAsync(FileExtensionModel model);
+    bool? FileExtensionExistsAsync(int id);
 }
